0ed29e041643a7f68506e6815239663e567766fd
vincentwang1517 <vin84526@gmail.com>
2023-03-18 16:00:12 -0400

    Removed

--- Zombie-Moon-Buggy/cs6501_run_zombie-moon-buggy.sh
+++
@@ -1,1 +0,0 @@
-pin -t ./obj-intel64/cs6501_proj1.so -- /mnt/c/Users/Surface/Desktop/UVA/SoftwareSecurity/CS-6501-Software-Security-via-Program-Analysis/Zombie-Moon-Buggy/moon-buggy-master/moon-buggy-org
